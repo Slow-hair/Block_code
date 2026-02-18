0149bcea41ac3b162cabe3fbc21098366b344438
Slow-hair <iiusovi2@gmail.com>
2026-02-18 12:56:17 +0700

    Вынес скрипты js в отдельный файл

---
+++ script_1.js
@@ -0,0 +1,24 @@
+function move(self) {
+            self.dataTransfer.setData('text/plain', self.target.outerHTML);
+        }
+        
+        function drop(self) {
+            self.preventDefault();
+            
+            let html = self.dataTransfer.getData('text/plain');
+            let temp = document.createElement('div');
+            temp.innerHTML = html;
+            let new_block = temp.firstChild;
+                       
+            let target = self.target;
+            
+            if (target.id == 'block_zone') {
+                target.appendChild(new_block);
+            }
+            else {
+                let box = document.createElement('div');
+                box.style.marginLeft = '20px';
+                box.appendChild(new_block);
+                target.appendChild(box);
+            }
+        }
