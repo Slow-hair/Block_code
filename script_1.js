4afacae3a441e0d478656f5eb563fd2098662160
Slow-hair <iiusovi2@gmail.com>
2026-02-22 12:46:45 +0700

    Добавил кнопку и зону вывода

--- script_1.js
+++ script_1.js
@@ -1,24 +1,34 @@
-function move(self) {
-            self.dataTransfer.setData('text/plain', self.target.outerHTML);
-        }
-        
-        function drop(self) {
-            self.preventDefault();
-            
-            let html = self.dataTransfer.getData('text/plain');
-            let temp = document.createElement('div');
-            temp.innerHTML = html;
-            let new_block = temp.firstChild;
-                       
-            let target = self.target;
-            
-            if (target.id == 'block_zone') {
-                target.appendChild(new_block);
-            }
-            else {
-                let box = document.createElement('div');
-                box.style.marginLeft = '20px';
-                box.appendChild(new_block);
-                target.appendChild(box);
-            }
-        }       
+function move(event) {
+    event.dataTransfer.setData('text/plain', event.target.outerHTML);
+}
+
+function drop(event) {
+    event.preventDefault();
+    
+    let html = event.dataTransfer.getData('text/plain');
+    let temp = document.createElement('div');
+    temp.innerHTML = html;
+    let new_block = temp.firstChild;
+    
+    new_block.removeAttribute('draggable');
+    new_block.style.cursor = 'default';
+                   
+    let target = event.target;
+    
+    if (target.id == 'block_zone') {
+        target.appendChild(new_block);
+    } else {
+        let box = document.createElement('div');
+        box.style.marginLeft = '20px';
+        box.appendChild(new_block);
+        target.appendChild(box);
+    }
+}
+
+function runCode() {
+    const blockZone = document.getElementById('block_zone');
+    const resultDisplay = document.getElementById('result_display');
+    
+    resultDisplay.innerHTML = '';
+
+}
